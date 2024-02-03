657f4d0d2d073ad8e4b46df9ddd8a4222a262613
Piotr Kielak <p.kielak@outlook.com>
2024-02-03 21:58:30 +0100

    update packages, change markdown wiki link plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,22 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import wikiLinkPlugin from "@portaljs/remark-wiki-link";
+import { wikiLinkPlugin } from "remark-wiki-link";
 import react from "@astrojs/react";
-const pageUrlPathPrefix = "notes/";
-
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), react()],
   markdown: {
-    remarkPlugins: [[wikiLinkPlugin, {
-      pathFormat: "obsidian-absolute",
-      wikiLinkResolver: slug => [pageUrlPathPrefix + slug]
-    }]]
-  }
+    remarkPlugins: [
+      [
+        wikiLinkPlugin,
+        {
+          hrefTemplate: (permalink) => `/notes/${permalink}`,
+          aliasDivider: "|",
+        },
+      ],
+    ],
+  },
 });
