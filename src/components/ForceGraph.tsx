727a9a4f9515b1b66827c4aa0b31c03b5da93a59
Piotr Kielak <p.kielak@outlook.com>
2024-02-05 00:25:26 +0100

    move graph to separate page, remove notes index page

--- src/components/ForceGraph.tsx
+++ src/components/ForceGraph.tsx
@@ -1,41 +1,42 @@
 import type React from "react";
+import { useRef } from "react";
 import ForceGraph2D, {
   type ForceGraphMethods,
   type GraphData,
 } from "react-force-graph-2d";
 
-import useWindowSize from "../hooks/useWindowSize";
-import { useRef } from "react";
+import "../styles/global.css";
 
-const ForceGraph: React.FC<{ data: GraphData }> = ({ data }) => {
+const ForceGraph: React.FC<{
+  data: GraphData;
+  height?: number;
+  padding?: number;
+}> = ({ data, height, padding }) => {
   const forceGraphRef = useRef<ForceGraphMethods>();
-  const windowSize = useWindowSize();
 
   const fitGraph = () => {
     if (forceGraphRef.current) {
-      forceGraphRef.current.zoomToFit(0, 32);
+      forceGraphRef.current.zoomToFit(0, padding);
     }
   };
 
-  if (!windowSize) return null;
-
-  const [, windowHeight] = windowSize;
-
   return (
-    <ForceGraph2D
-      ref={forceGraphRef}
-      height={windowHeight * 0.3}
-      graphData={data}
-      linkColor={() => "#8FBCBB"}
-      nodeColor={() => "#88C0D0"}
-      onNodeClick={(node) => {
-        window.location.href = `/notes/${node.id}`;
-      }}
-      onEngineStop={fitGraph}
-      warmupTicks={100}
-      cooldownTicks={0}
-      enableNodeDrag={false}
-    />
+    <div className="graph">
+      <ForceGraph2D
+        ref={forceGraphRef}
+        graphData={data}
+        {...(height ? { height } : {})}
+        linkColor={() => "#8FBCBB"}
+        nodeColor={() => "#88C0D0"}
+        onNodeClick={(node) => {
+          window.location.href = `/notes/${node.id}`;
+        }}
+        onEngineStop={fitGraph}
+        warmupTicks={100}
+        cooldownTicks={0}
+        enableNodeDrag={false}
+      />
+    </div>
   );
 };
 
