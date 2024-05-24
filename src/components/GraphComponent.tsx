8fa8feb3db05602893dc9491e1b58fc500b336ff
Piotr Kielak <p.kielak@outlook.com>
2024-05-24 18:43:08 +0200

    graph component adjustments

--- src/components/GraphComponent.tsx
+++ src/components/GraphComponent.tsx
@@ -1,18 +1,23 @@
 import type React from "react";
-
-import "../styles/global.css";
+import type { Sigma } from "sigma";
 import type { SerializedGraph } from "graphology-types";
+import { useInView } from "react-intersection-observer";
 import Graph from "graphology";
 import {
   ControlsContainer,
   FullScreenControl,
   SearchControl,
   SigmaContainer,
+  useFullScreen,
+  useLoadGraph,
   useRegisterEvents,
 } from "@react-sigma/core";
 import { useEffect, useMemo, useState } from "react";
 import { DEFAULT_SETTINGS } from "sigma/settings";
 import { navigate } from "astro:transitions/client";
+
+import network from "../../public/network.svg";
+import "../styles/graph.css";
 
 const sigmaSettings = {
   allowInvalidContainer: true,
@@ -33,11 +38,39 @@ const GraphEvents: React.FC = () => {
   return null;
 };
 
-const GraphComponent: React.FC<{
+export const LoadGraph: React.FC<{
   data: SerializedGraph;
 }> = ({ data }) => {
   const graph = useMemo(() => Graph.from(data), [data]);
+
+  const loadGraph = useLoadGraph();
+
+  useEffect(() => {
+    loadGraph(graph);
+  }, [loadGraph]);
+
+  return null;
+};
+
+const ShowGraphButton = () => {
+  const { toggle } = useFullScreen();
+
+  return (
+    <button className="graph-button" onClick={toggle} title="Show graph">
+      <img src={network.src} alt="network icon" />
+    </button>
+  );
+};
+
+const GraphComponent: React.FC<{
+  data: SerializedGraph;
+}> = ({ data }) => {
+  const [sigma, setSigma] = useState<Sigma | null>(null);
   const [isFullscreen, setIsFullscreen] = useState(false);
+  const { ref, inView } = useInView({
+    triggerOnce: false,
+    initialInView: true,
+  });
 
   useEffect(() => {
     const handleFullscreenChange = () => {
@@ -51,18 +84,32 @@ const GraphComponent: React.FC<{
     };
   }, []);
 
+  useEffect(() => {
+    sigma?.scheduleRefresh();
+  }, [inView]);
+
   return (
-    <SigmaContainer settings={sigmaSettings} graph={graph}>
-      <GraphEvents />
-      {isFullscreen && (
-        <ControlsContainer position="top-right">
-          <SearchControl />
+    <div className="graph-wrapper">
+      <SigmaContainer
+        ref={setSigma}
+        className={`graph ${inView ? "" : "graph--scroll"}`}
+        settings={sigmaSettings}
+      >
+        <GraphEvents />
+        <LoadGraph data={data} />
+        {isFullscreen ? (
+          <ControlsContainer position="top-right">
+            <SearchControl />
+          </ControlsContainer>
+        ) : (
+          !inView && <ShowGraphButton />
+        )}
+        <ControlsContainer position="bottom-right">
+          <FullScreenControl />
         </ControlsContainer>
-      )}
-      <ControlsContainer position="bottom-right">
-        <FullScreenControl />
-      </ControlsContainer>
-    </SigmaContainer>
+      </SigmaContainer>
+      <div ref={ref}></div>
+    </div>
   );
 };
 
