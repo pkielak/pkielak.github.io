72f9fa0ec961a89162b23cce92469c0236cd8257
Piotr Kielak <p.kielak@outlook.com>
2024-10-13 02:43:50 +0200

    content changes, style changes, add deploy script

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://p.kielak.github,io",
   integrations: [mdx(), sitemap(), react()],
   markdown: {
     remarkPlugins: [
