4e09612872a414c4de3e3f8eaefc1d4de84b139c
Piotr Kielak <p.kielak@outlook.com>
2026-08-23 16:13:04 +0200

    get rid of react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { wikiLinkPlugin } from "remark-wiki-link";
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://pkielak.me",
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     remarkPlugins: [
       [
